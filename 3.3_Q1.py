1ae2116e465a451e65816163693a3046f00747a4
Nicholas Perez <nicholas@victor22.ksd.org>
2019-11-05 12:51:27 -0800

    AA

---
+++ 3.3_Q1.py
@@ -0,0 +1,10 @@
+#payday code
+
+day = int(input("enter a day: "))
+print() #space
+
+if (day == 15 or day == 30):
+	print("It's payday!")
+else:
+	print("sorry, not a payday")
+
---
+++ password_checker.py
@@ -0,0 +1,9 @@
+#password checker
+
+a = str(input("enter a password: "))
+
+if (a == "Ada Lovelace"):
+	print("Correct!")
+	
+if (a != "Ada Lovelace"):
+	print("Not Correct")
